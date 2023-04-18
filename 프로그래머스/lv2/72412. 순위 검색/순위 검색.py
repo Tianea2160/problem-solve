61c63e93267f42e98e5a64f5c9fdf1f4f3c962e3
Jo Hyun Jun <73744183+Tianea2160@users.noreply.github.com>
2023-04-18 18:46:53 +0900

    [level 2] Title: 순위 검색, Time: 615.45 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -5,30 +5,35 @@ from bisect import bisect_left
 def solution(information, querys):
     answer = []
     
-    dic = defaultdict(list)
+    # 해당 조건에 맞는 지원자가 '몇명'인지 쉽게 할 수 있는 도구 만들기
+    
+    # 검색할 수 있는 모든 조건의 경우를 돌면서 해당 조건을 key로 사용하고, 해당 조건을 기준으로 몸무게를 넣는다.
+    data = defaultdict(list)
+    
     for info in information:
-        info = info.split()
-        condition = info[:-1]  
-        score = int(info[-1])
+        words = info.split()
+        condition = words[:-1]
+        score = int(words[-1])
+        
         for i in range(5):
-            case = list(combinations([0,1,2,3], i))
+            case = combinations([0,1,2,3], i)
             for c in case:
                 tmp = condition.copy()
-                for idx in c:
-                    tmp[idx] = "-"
-                key = ''.join(tmp)
-                dic[key].append(score) 
-    for value in dic.values():
+                for idx in c: tmp[idx] = "-"
+                key = "".join(tmp)
+                data[key].append(score)
+        
+        # for k in data: print(k, data[k])
+    for value in data.values():
         value.sort()
-    
+        
     for query in querys:
-        query = query.replace("and", "")
-        query = query.split()
-        target_key = "".join(query[:-1])
-        target_score = int(query[-1])
-        target_list  = dic[target_key]
-        
-        idx = bisect_left(target_list, target_score)
-        answer.append(len(target_list) - idx)
-        
+        words = query.replace("and", "").split()
+        condition = words[:-1]
+        target = int(words[-1])
+        key = "".join(condition)
+        scores = data[key]
+        idx = bisect_left(scores, target)
+        answer.append(len(scores) - idx)
+    
     return answer
