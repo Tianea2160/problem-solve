af0c9f3356dc1fa23251d4b2dc34519289621483
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-06-08 16:45:46 +0900

    [Silver V] Title: 배열 합치기, Time: 2204 ms, Memory: 486240 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.kt
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.kt
@@ -1,12 +1,11 @@
-
 fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
     val firstArray = readln().split(" ").map { it.toInt() }.toIntArray()
     val secondArray = readln().split(" ").map { it.toInt() }.toIntArray()
     val result = mutableListOf<Int>()
-    var i = 0
-    var j = 0
+    var (i, j) = 0 to 0
 
+    // 시간 복잡도 O(2n) == O(n)
     while (i < n && j < m) {
         if (firstArray[i] < secondArray[j]) {
             result.add(firstArray[i])
@@ -17,12 +16,12 @@ fun main() {
         }
     }
 
-    if (i == n) {
+    if (i == n) { // 첫번째 배열의 끝에 먼저 도착한 경우
         while (j < m) {
             result.add(secondArray[j])
             j++
         }
-    } else { //  j == m
+    } else { //  j == m, 두번째 배열의 끝에 먼저 도착한 경우
         while (i < n) {
             result.add(firstArray[i])
             i++
@@ -30,4 +29,3 @@ fun main() {
     }
     println(result.joinToString(" "))
 }
-
