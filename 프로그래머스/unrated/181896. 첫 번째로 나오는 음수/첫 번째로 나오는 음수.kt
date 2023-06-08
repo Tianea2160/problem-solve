2e68a9cf3750bcb2f065e2cdac3757f409718f19
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-06-09 00:56:59 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.kt
@@ -1,10 +1,3 @@
 class Solution {
-    fun solution(nums: IntArray): Int {
-        for(i in nums.indices){
-            if(nums[i] < 0){
-                return i
-            }
-        }
-        return -1
-    }
+    fun solution(nums: IntArray) = nums.indexOfFirst{ it < 0}
 }
