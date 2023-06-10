86953f022bbaef71b65be8e43a3e914dd77f508d
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-06-11 05:38:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer = 0
+        var cur = n
+        while(cur > 0){
+            answer += cur % 10
+            cur /= 10
+        }
+        return answer
+    }
+}
