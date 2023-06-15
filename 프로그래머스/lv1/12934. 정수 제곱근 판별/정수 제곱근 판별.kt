efe4e906a55f4cc156d1fe212676d5145afe0407
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-06-15 23:00:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -1,9 +1,9 @@
 class Solution {
     fun solution(n: Long): Long {
-        for(i in 1..(n/2+1)){
-            if(i*i == n){
-                return (i+1)*(i+1)
-            }
+        for(i in 1..n.toLong()){
+            val cur = i*i
+            if(cur > n) break
+            if(cur == n) return (i+1)*(i+1)
         }
         return -1
     }
