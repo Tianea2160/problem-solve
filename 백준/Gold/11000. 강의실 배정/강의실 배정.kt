abccc2995ee5240575a153823a45f420c497a2da
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-07-03 12:13:41 +0900

    [Gold V] Title: 강의실 배정, Time: 1216 ms, Memory: 201260 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.kt
@@ -0,0 +1,36 @@
+import java.util.PriorityQueue
+import kotlin.math.max
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+    val times: Array<Pair<Int, Int>> = Array(n) {
+        val nums = br.readLine().split(" ").map { it.toInt() }
+        nums[0] to nums[1]
+    }
+    br.close()
+
+    times.sortBy { time -> time.first } // 시작시간을 기준으로 오름차순
+
+    val queue = PriorityQueue<Pair<Int, Int>> { a, b -> a.second - b.second }
+    var answer = 0
+
+    for (time in times) {
+        if (queue.isEmpty()) {
+            queue.add(time)
+            answer = max(answer, queue.size)
+            continue
+        }
+        val cur = queue.poll()
+
+        if (cur.second <= time.first) {
+            queue.add(time)
+        } else {
+            queue.add(time)
+            queue.add(cur)
+        }
+        answer = max(answer, queue.size)
+    }
+
+    println(answer)
+}
