0e67e8032499b8a464935ceefc3402a13c00ab7f
Jo Hyun Jun <hyunjun2087@gmail.com>
2023-12-03 23:55:31 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 65.69 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,38 +1,43 @@
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
 def solution(land):
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
     answer = 0
     n, m = len(land), len(land[0])
     columns = [0 for i in range(m)]
     visit = [[False for i in range(m)] for j in range(n)]
     
+    # pre processing
     for i in range(n):
         for j in range(m):
-            if land[i][j] == 1 and not visit[i][j]:
-                # find count
+            if land[i][j] == 0:
+                continue
+        
+            if not visit[i][j]:
                 visit[i][j] = True
-                queue = [(i, j)]
-                pos = [(i, j)]
+                queue, pos = [(i, j)], [(i, j)]
                 count = 1
-
+                cols = set([j])
+                
+                # search oil count
                 while queue:
                     y, x = queue.pop()
 
-                    for idx in range(4):
-                        ny, nx = dy[idx] + y, dx[idx] + x
+                    for k in range(4):
+                        ny, nx = dy[k] + y, dx[k] + x
 
                         if 0 <= ny < n and 0 <= nx < m:
-                            if land[ny][nx] == 1 and not visit[ny][nx]:
+                            if land[ny][nx] == 0:
+                                continue
+
+                            if not visit[ny][nx]:
                                 visit[ny][nx] = True    
-                                count += 1  
+                                count += 1
                                 queue.append((ny, nx))
                                 pos.append((ny, nx))
+                                cols.add(nx)
                 
-                cols = set([b for a, b in pos])
-                
-                # add
+                # add oil count where oil colum index
                 for idx in cols:
                     columns[idx] += count
-
+    
     return max(columns)
