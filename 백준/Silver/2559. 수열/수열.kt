29d398f9bad2be19a3e89d18a8fb1357a437c178
Jo Hyun Jun <hyunjun2087@gmail.com>
2024-01-14 15:51:36 +0900

    [Silver III] Title: 수열, Time: 352 ms, Memory: 32764 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.kt
+++ 백준/Silver/2559. 수열/수열.kt
@@ -1,14 +1,20 @@
+import kotlin.math.max
+
 fun main() {
     val br = System.`in`.bufferedReader()
     val (n, k) = br.readLine().split(" ").map { it.toInt() }
     val arr = br.readLine().split(" ").map { it.toLong() }.toLongArray()
-    val results = mutableListOf<Long>()
-    results.add((0 until k).sumOf { arr[it] })
+    
+    // 첫 k일 동안의 온도 합으로 answer 초기화
+    var answer = (0 until k).sumOf { arr[it] }
+    var cur = answer
 
     for (i in k until n) {
-        results.add(results.last() - arr[i - k] + arr[i])
+        cur -= arr[i - k]
+        cur += arr[i]
+        answer = max(cur, answer)
     }
 
-    println(results.max())
+    println(answer)
     br.close()
 }
