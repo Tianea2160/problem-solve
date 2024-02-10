774941734da3feffffed0de153acc3d909707a1d
Jo Hyun Jun <hyunjun2087@gmail.com>
2024-02-10 12:15:20 +0900

    [Platinum V] Title: 히스토그램, Time: 328 ms, Memory: 24924 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.kt
@@ -0,0 +1,29 @@
+import java.util.*
+import kotlin.math.max
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
+    val arr = LongArray(100_005) { 0L }
+
+    for (i in 1..n) {
+        arr[i] = br.readLine().toLong()
+    }
+    var answer = 0L
+    val stack = Stack<Int>()
+
+    stack.add(0)
+
+    for (i in 1..n + 1) {
+        while (stack.isNotEmpty() && arr[stack.peek()] > arr[i]) {
+            val c = stack.peek()
+            stack.pop()
+            val width = i - stack.peek() - 1
+            answer = max(answer, width * arr[c])
+        }
+        stack.add(i)
+    }
+
+    print(answer)
+    br.close()
+}
