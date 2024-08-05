44af29c561c7b51cd9ca2eb78d2b5745bea341e8
Jo Hyun Jun <hyunjun2087@gmail.com>
2024-08-05 22:45:25 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 848 ms, Memory: 140616 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.kt
@@ -1,15 +1,41 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
+
     val str = br.readLine()
     val n = br.readLine().toInt()
 
-    for (i in 0 until n) {
-        val (a, b, c) = br.readLine().split(" ")
-        val (start, end) = b.toInt() to c.toInt()
+    val map = mutableMapOf<Char, IntArray>()
 
-        val count = str.substring(start..end).count { it == a[0] }
-        bw.write("$count\n")
+    for (c in 'a'..'z') {
+        map[c] = IntArray(size = str.length)
+    }
+
+    for ((i, c) in str.withIndex()) {
+        if (i == 0) {
+            map[c]!![i]++
+        } else {
+            // 이전 값 불러오기
+            for (key in map.keys) {
+                map[key]!![i] = map[key]!![i - 1]
+            }
+
+            map[c]!![i]++
+        }
+    }
+
+    for (i in 0 until n) {
+        val (char, u, v) = br.readLine().split(" ")
+        val start = u.toInt()
+        val end = v.toInt()
+
+        val answer = if (start == 0) {
+            map[char.first()]!![end]
+        } else {
+            map[char.first()]!![end] - map[char.first()]!![start - 1]
+        }
+        
+        bw.write("$answer\n")
     }
 
     bw.close()
